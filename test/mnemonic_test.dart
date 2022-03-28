9e4c62b1f553949e97f593064e1b3d2d43c61e29
DESKTOP-V9IK3JQ\123 <252826098@qq.com>
2022-03-28 17:00:44 +0800

    Modify the code

--- test/mnemonic_test.dart
+++ test/mnemonic_test.dart
@@ -3,13 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import '../lib/flutter_trust_wallet_core.dart';
 
 void main() {
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-  group('hDWallet.mnemonic test',(){
+  FlutterTrustWalletCore.init();
     test('hDWallet.mnemonic test',(){
       final hDwallet = HDWallet.createWithMnemonic('whale agree trap bar noodle marriage rally pride catch consider desert program');
       final mnemonic = hDwallet.mnemonic();
       expect(mnemonic, 'whale agree trap bar noodle marriage rally pride catch consider desert program');
     });
-  });
 }
