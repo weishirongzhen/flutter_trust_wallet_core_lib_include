22275e324e27393ede768132a09143a553b20fca
DESKTOP-V9IK3JQ\123 <252826098@qq.com>
2022-03-29 16:46:03 +0800

    change the code
    Changed the coinType in the getExtendedPublicKey method

--- test/Ethereum/getExtendedPublicKey_test.dart
+++ test/Ethereum/getExtendedPublicKey_test.dart
@@ -5,8 +5,8 @@ void main(){
   FlutterTrustWalletCore.init();
     test('getExtendedPublicKey of bip32',(){
       final hDWallet = HDWallet.createWithMnemonic('liberty tape tattoo inch season mushroom axis immune view novel drill bus');
-      final publickey = hDWallet.getExtendedPublicKey(32,195,0x0488b21e);
-      expect(publickey, 'xpub6CoEXKF8F6ar8gndPjqB6KLYgs4Kzgv74XcSQZPLuFpuNjvBaFY5LhbPtqSoghA5WXFPpB3264qyPhXem1PA4zLaSxDQjRTKYTXkcfYwiHZ');
+      final publickey = hDWallet.getExtendedPublicKey(32,60,0x0488b21e);
+      expect(publickey, 'xpub6DRPmRej61UJjbu4m4jYjyKE8drQBSYCGNqf8yq5Kgbc2zVB71D4bnAVhSy7iciHFv3cWPZvUdYRfS9dn3EY4A6y5vG9TgkZyzmyKCJfMwe');
     });
 
 }
