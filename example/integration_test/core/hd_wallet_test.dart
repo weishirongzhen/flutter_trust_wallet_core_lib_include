25285df1e4a8ab118ac5a2acd064d081258a65d6
memtopia <memtopia@gmail.com>
2022-03-30 11:37:19 +0800

    add hd wallet test case empty file

---
+++ example/integration_test/core/hd_wallet_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
+
+void main() {
+
+}
--- example/integration_test/flutter_trust_wallet_core_test.dart
+++ example/integration_test/flutter_trust_wallet_core_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
 import './core/mnemonic_test.dart' as mnemonic;
+import './core/hd_wallet_test.dart' as hdWallet;
 
 void main() {
 
@@ -10,6 +11,6 @@ void main() {
     FlutterTrustWalletCore.init();
   });
   mnemonic.main();
-
+  hdWallet.main();
 
 }
