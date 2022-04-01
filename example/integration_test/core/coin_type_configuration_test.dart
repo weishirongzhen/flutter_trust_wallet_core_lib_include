d91e10b9659845786fd786b7d6e825a32be1a65c
yue <2324408609@qq.com>
2022-04-01 15:58:39 +0800

    update tests: bitcoin_address_test.dart\coin_type_configuration_test.dart

--- example/integration_test/core/coin_type_configuration_test.dart
+++ example/integration_test/core/coin_type_configuration_test.dart
@@ -4,52 +4,52 @@ import 'package:flutter_trust_wallet_core/flutter_trust_wallet_core.dart';
 void main() {
   FlutterTrustWalletCore.init();
   group('CoinTypeConfiguration.getSymbol test', () {
-    test('BTC', () {
+    test('symbol will be BTC', () {
       final symbol = CoinTypeConfiguration.getSymbol(0);
       expect(symbol, 'BTC');
     });
-    test('ETH', () {
+    test('symbol will be ETH', () {
       final symbol = CoinTypeConfiguration.getSymbol(60);
       expect(symbol, 'ETH');
     });
 
-    test('TRX', () {
+    test('symbol will be TRX', () {
       final symbol = CoinTypeConfiguration.getSymbol(195);
       expect(symbol, 'TRX');
     });
   });
 
   group('CoinTypeConfiguration.getDecimals test', () {
-    test('BTC', () {
+    test('BTC decimal will be 8', () {
       final Decimal = CoinTypeConfiguration.getDecimals(0);
-      expect(Decimal == 8, true);
+      expect(Decimal, 8);
     });
-    test('ETH', () {
+    test('ETH decimal will be 18', () {
       final Decimal = CoinTypeConfiguration.getDecimals(60);
-      expect(Decimal == 18, true);
+      expect(Decimal, 18);
     });
-    test('TRX', () {
+    test('TRX decimal will be 6', () {
       final Decimal = CoinTypeConfiguration.getDecimals(195);
-      expect(Decimal == 6, true);
+      expect(Decimal, 6);
     });
   });
 
   group('CoinTypeConfiguration.getAccountURL test', () {
-    test('BTC', () {
+    test('AccountURL is BTC', () {
       final url = CoinTypeConfiguration.getAccountURL(0,
           '0607f62530b68cfcc91c57a1702841dd399a899d0eecda8e31ecca3f52f01df2');
       expect(url,
           'https://blockchair.com/bitcoin/address/0607f62530b68cfcc91c57a1702841dd399a899d0eecda8e31ecca3f52f01df2');
     });
 
-    test('ETH', () {
+    test('AccountURL is ETH', () {
       final url = CoinTypeConfiguration.getAccountURL(60,
           '0x9edaf0f7d9c6629c31bbf0471fc07d696c73b566b93783f7e25d8d5d2b62fa4f');
       expect(url,
           'https://etherscan.io/address/0x9edaf0f7d9c6629c31bbf0471fc07d696c73b566b93783f7e25d8d5d2b62fa4f');
     });
 
-    test('TRX', () {
+    test('AccountURL is TRX', () {
       final url = CoinTypeConfiguration.getAccountURL(195,
           'c0a421c6ade63295695b1ad6569612cbfc754982d716a5ada5b08ec4e2e6d64e');
       expect(url,
@@ -58,21 +58,21 @@ void main() {
   });
 
   group('CoinTypeConfiguration.getTransactionURL test', () {
-    test('BTC', () {
+    test('TransactionURL is BTC', () {
       final url = CoinTypeConfiguration.getTransactionURL(0,
           '0607f62530b68cfcc91c57a1702841dd399a899d0eecda8e31ecca3f52f01df2');
       expect(url,
           'https://blockchair.com/bitcoin/transaction/0607f62530b68cfcc91c57a1702841dd399a899d0eecda8e31ecca3f52f01df2');
     });
 
-    test('ETH', () {
+    test('TransactionURL is ETH', () {
       final url = CoinTypeConfiguration.getTransactionURL(60,
           '0x9edaf0f7d9c6629c31bbf0471fc07d696c73b566b93783f7e25d8d5d2b62fa4f');
       expect(url,
           'https://etherscan.io/tx/0x9edaf0f7d9c6629c31bbf0471fc07d696c73b566b93783f7e25d8d5d2b62fa4f');
     });
 
-    test('TRX', () {
+    test('TransactionURL is TRX', () {
       final url = CoinTypeConfiguration.getTransactionURL(195,
           'c0a421c6ade63295695b1ad6569612cbfc754982d716a5ada5b08ec4e2e6d64e');
       expect(url,
@@ -81,34 +81,34 @@ void main() {
   });
 
   group('CoinTypeConfiguration.getID test', () {
-    test('BTC', () {
+    test('ID will be bitcoin', () {
       final id = CoinTypeConfiguration.getID(0);
       expect(id, 'bitcoin');
     });
 
-    test('ETH', () {
+    test('ID will be ethereum', () {
       final id = CoinTypeConfiguration.getID(60);
       expect(id, 'ethereum');
     });
 
-    test('TRX', () {
+    test('ID will be tron', () {
       final id = CoinTypeConfiguration.getID(195);
       expect(id, 'tron');
     });
   });
 
   group('CoinTypeConfiguration.getName test', () {
-    test('BTC', () {
+    test('Name will be BTC', () {
       final name = CoinTypeConfiguration.getName(0);
       expect(name, 'Bitcoin');
     });
 
-    test('ETH', () {
+    test('Name will be Ethereum', () {
       final name = CoinTypeConfiguration.getName(60);
       expect(name, 'Ethereum');
     });
 
-    test('TRX', () {
+    test('Name will be Tron', () {
       final name = CoinTypeConfiguration.getName(195);
       expect(name, 'Tron');
     });
