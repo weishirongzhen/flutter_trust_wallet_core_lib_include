e3f925b8ade155f09f665d1df84d4c3f8abe0cf6
weixuefeng <weixuefeng1018@gmail.com>
2022-07-08 11:59:10 +0800

    fix: bitcoin example bug

--- example/lib/bitcoin_address_example.dart
+++ example/lib/bitcoin_address_example.dart
@@ -10,24 +10,28 @@ import 'package:flutter_trust_wallet_core_example/base_example.dart';
 class BitcoinAddressExample extends BaseExample {
   final HDWallet wallet;
 
-  const BitcoinAddressExample(this.wallet, {Key? key}) : super('Bitcoin Address', key: key);
+  const BitcoinAddressExample(this.wallet, {Key? key})
+      : super('Bitcoin Address', key: key);
 
   @override
   _BitcoinAddressExampleState createState() => _BitcoinAddressExampleState();
 }
 
-class _BitcoinAddressExampleState extends BaseExampleState<BitcoinAddressExample> {
+class _BitcoinAddressExampleState
+    extends BaseExampleState<BitcoinAddressExample> {
   @override
   void initState() {
     super.initState();
-    int coin = TWCoinType.TWCoinTypeBitcoinTestnet;
+    int coin = TWCoinType.TWCoinTypeBitcoin;
     final privakye2 = widget.wallet.getKey(coin, "m/44'/0'/0'/0/0");
     logger.d(hex.encode(privakye2.data()));
     final publicKey2 = privakye2.getPublicKeySecp256k1(true);
-    logger.d(widget.wallet.getExtendedPublicKey(TWPurpose.TWPurposeBIP44, coin, TWHDVersion.TWHDVersionTPUB));
+    logger.d(widget.wallet.getExtendedPublicKey(
+        TWPurpose.TWPurposeBIP44, coin, TWHDVersion.TWHDVersionTPUB));
     final bitcoinAddress = BitcoinAddress.createWithPublicKey(publicKey2, coin);
     logger.d(bitcoinAddress.description());
-    final segwitAddress = SegwitAddress.createWithPublicKey(HRP.Bitcoin, publicKey2);
+    final segwitAddress =
+        SegwitAddress.createWithPublicKey(HRP.Bitcoin, publicKey2);
     logger.d(segwitAddress.description());
     final address2 = AnyAddress.createWithPublicKey(publicKey2, 0);
     logger.d("address2  = ${address2.description()}");
@@ -39,13 +43,15 @@ class _BitcoinAddressExampleState extends BaseExampleState<BitcoinAddressExample
     final bitcoinAddress2 = BitcoinAddress.createWithData(data);
     logger.d(bitcoinAddress2.description());
 
-
-    final bip84Privakey = widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeBitcoin);
-    final wif = WIF.encode(hex.encode(bip84Privakey.data()), TWCoinType.TWCoinTypeBitcoin);
+    final bip84Privakey =
+        widget.wallet.getKeyForCoin(TWCoinType.TWCoinTypeBitcoin);
+    final wif = WIF.encode(
+        hex.encode(bip84Privakey.data()), TWCoinType.TWCoinTypeBitcoin);
     logger.d("bip84 origin privakey = ${hex.encode(bip84Privakey.data())}");
     logger.d("bip84 wif privakey = $wif");
 
-    final keystore = StoredKey.importHDWallet(widget.wallet.mnemonic(), "wtf", "123", TWCoinType.TWCoinTypeBitcoin);
+    final keystore = StoredKey.importHDWallet(
+        widget.wallet.mnemonic(), "wtf", "123", TWCoinType.TWCoinTypeBitcoin);
     logger.d("keystore json = ${keystore?.exportJson()}");
   }
 }
--- example/lib/bitcoin_transaction_example.dart
+++ example/lib/bitcoin_transaction_example.dart
@@ -9,17 +9,20 @@ import 'package:fixnum/fixnum.dart' as $fixnum;
 class BitcoinTransactionExample extends BaseExample {
   final HDWallet wallet;
 
-  const BitcoinTransactionExample(this.wallet, {Key? key}) : super('Bitcoin Transaction', key: key);
+  const BitcoinTransactionExample(this.wallet, {Key? key})
+      : super('Bitcoin Transaction', key: key);
 
   @override
-  _BitcoinTransactionExampleState createState() => _BitcoinTransactionExampleState();
+  _BitcoinTransactionExampleState createState() =>
+      _BitcoinTransactionExampleState();
 }
 
-class _BitcoinTransactionExampleState extends BaseExampleState<BitcoinTransactionExample> {
+class _BitcoinTransactionExampleState
+    extends BaseExampleState<BitcoinTransactionExample> {
   @override
   void initState() {
     super.initState();
-    int coin = TWCoinType.TWCoinTypeBitcoinTestnet;
+    int coin = TWCoinType.TWCoinTypeBitcoin;
     final addressBtc = widget.wallet.getAddressForCoin(coin);
     final toAddress = "tb1qks8l9haxjszn9r6yf2dm65ed3w6wmz85r379ms";
     final changeAddress = "mvcJcHN3ZhELVVhncfjARUMtfRELwhGXgL";
@@ -35,29 +38,44 @@ class _BitcoinTransactionExampleState extends BaseExampleState<BitcoinTransactio
         Bitcoin.UnspentTransaction(
           amount: $fixnum.Int64.parseInt('20000'),
           outPoint: Bitcoin.OutPoint(
-            hash: hex.decode('1b23757cdc023b3ac9f033522abb9f845815b65cce1e25411e8ad950899c0e71').reversed.toList(),
+            hash: hex
+                .decode(
+                    '1b23757cdc023b3ac9f033522abb9f845815b65cce1e25411e8ad950899c0e71')
+                .reversed
+                .toList(),
             index: 0,
             sequence: 4294967295,
           ),
-          script: BitcoinScript.lockScriptForAddress(addressBtc, coin).data().toList(),
+          script: BitcoinScript.lockScriptForAddress(addressBtc, coin)
+              .data()
+              .toList(),
         ),
         Bitcoin.UnspentTransaction(
           amount: $fixnum.Int64.parseInt('20000'),
           outPoint: Bitcoin.OutPoint(
-            hash: hex.decode('7611002ff116fad20ef12ad30010a07d5b25edf37209504dd42a6a4c5c27aa75').reversed.toList(),
+            hash: hex
+                .decode(
+                    '7611002ff116fad20ef12ad30010a07d5b25edf37209504dd42a6a4c5c27aa75')
+                .reversed
+                .toList(),
             index: 0,
             sequence: 4294967295,
           ),
-          script: BitcoinScript.lockScriptForAddress(addressBtc, coin).data().toList(),
+          script: BitcoinScript.lockScriptForAddress(addressBtc, coin)
+              .data()
+              .toList(),
         ),
       ],
       privateKey: [
         secretPrivateKeyBtc.data().toList(),
       ],
     );
-    final transactionPlan = Bitcoin.TransactionPlan.fromBuffer(AnySigner.signerPlan(signingInput.writeToBuffer(), coin).toList());
-    logger.d('availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
-    print('availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
+    final transactionPlan = Bitcoin.TransactionPlan.fromBuffer(
+        AnySigner.signerPlan(signingInput.writeToBuffer(), coin).toList());
+    logger.d(
+        'availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
+    print(
+        'availableAmount: ${transactionPlan.availableAmount} amount: ${transactionPlan.amount} fee: ${transactionPlan.fee} change: ${transactionPlan.change}');
     signingInput.plan = transactionPlan;
     signingInput.amount = transactionPlan.amount;
     final sign = AnySigner.sign(signingInput.writeToBuffer(), coin);
